9e851e465092b93d90e4c612dc2f515b690b59aa
mrx151 <~g402251907>
2019-10-16 09:29:53 +0800

    尝试commit

--- src/com/mrx/lst/HelloServelet.java
+++ src/com/mrx/lst/HelloServelet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ *  add git management
  * Servlet implementation class HelloServelet
  */
 @WebServlet("/HelloServelet")
