44dcc45e5ae5fa989842b06335caf1a1e778fd6d
mrx151 <402251907@qq.com>
2019-10-24 22:26:35 +0800

    day 6，扫描器

--- src/com/mrx/annotations/ScanClassUtil.java
+++ src/com/mrx/annotations/ScanClassUtil.java
@@ -1,16 +1,97 @@
 package com.mrx.annotations;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Enumeration;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class ScanClassUtil {
 	
-	public static Set<Class<?>> getClasses(String packagename){
+	public static Set<Class<?>> getClasses(String pack){
 		
-		Set<Class<?>> set = new LinkedHashSet();
+		Set<Class<?>> classes = new LinkedHashSet();
+		boolean recursive = true;//是否循环迭代
+		//获取包名，将包名的点号“.”换成“/”
+		String packageName = pack;
+		String packageDirName = packageName.replace('.', '/');
+		//定义一个枚举集合，循环处理这个目录下的things
+		Enumeration<URL> dirs;
+		try {
+			dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
+			//循环迭代下去
+			while(dirs.hasMoreElements()) {
+				URL url = dirs.nextElement();
+				String protocol = url.getProtocol();
+				if ("file".equals(protocol)) {
+					System.err.println("file类型的扫描");
+					// 获取包的物理路径
+					String filePath = URLDecoder.decode(url.getFile(),"utf-8");
+					// 以文件方式扫描整个包下的文件 并添加到集合中
+                    findAndAddClassesInPackageByFile(packageName, filePath,
+                            recursive, classes);
+				} else if ("jar".equals(protocol)) {
+					// 如果是jar包文件
+					// 定义一个JarFile
+					
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 		
-		
-		return set;
+		return classes;
 	}
+	
+    /**
+     * 以文件的形式来获取包下的所有Class
+     * 
+     * @param packageName
+     * @param packagePath
+     * @param recursive
+     * @param classes
+     */
+    public static void findAndAddClassesInPackageByFile(String packageName,
+            String packagePath, final boolean recursive, Set<Class<?>> classes) {
+        // 获取此包的目录 建立一个File
+        File dir = new File(packagePath);
+        // 如果不存在或者 也不是目录就直接返回
+        if (!dir.exists() || !dir.isDirectory()) {
+            // log.warn("用户定义包名 " + packageName + " 下没有任何文件");
+            return;
+        }
+        // 如果存在 就获取包下的所有文件 包括目录
+        File[] dirfiles = dir.listFiles(new FileFilter() {
+            // 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
+            public boolean accept(File file) {
+                return (recursive && file.isDirectory())
+                        || (file.getName().endsWith(".class"));
+            }
+        });
+        // 循环所有文件
+        for (File file : dirfiles) {
+            // 如果是目录 则继续扫描
+            if (file.isDirectory()) {
+                findAndAddClassesInPackageByFile(packageName + "."
+                        + file.getName(), file.getAbsolutePath(), recursive,
+                        classes);
+            } else {
+                // 如果是java类文件 去掉后面的.class 只留下类名
+                String className = file.getName().substring(0,
+                        file.getName().length() - 6);
+                try {
+                    // 添加到集合中去
+                    //classes.add(Class.forName(packageName + '.' + className));
+                     //经过回复同学的提醒，这里用forName有一些不好，会触发static方法，没有使用classLoader的load干净
+                    classes.add(Thread.currentThread().getContextClassLoader().loadClass(packageName + '.' + className));  
+                    } catch (ClassNotFoundException e) {
+                    // log.error("添加用户自定义视图类错误 找不到此类的.class文件");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
